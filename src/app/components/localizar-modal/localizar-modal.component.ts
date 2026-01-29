78b1f7aa794cfaea498f5fcf83136ea8a353a71e
Hans-el <halcivar3030@utm.edu.ec>
2026-01-28 22:50:17 -0500

    Mejoras UI y UX en localizar

--- src/app/components/localizar-modal/localizar-modal.component.ts
+++ src/app/components/localizar-modal/localizar-modal.component.ts
@@ -40,15 +40,18 @@ export class LocalizarModalComponent implements OnInit {
     });
   }
 
+  // Llama a este método cada vez que cambia el input
   onInputChange(): void {
     this.searchTerms.next(this.nombreFallecido);
   }
 
+  // Selecciona una sugerencia del autocompletado
   seleccionarSugerencia(sugerencia: string): void {
     this.nombreFallecido = sugerencia;
     this.sugerencias = [];
   }
 
+  // Método para buscar la ubicación por el nombre del fallecido
   buscar(): void {
     if (!this.nombreFallecido) {
       this.mensajeError = 'Por favor, ingrese el nombre del fallecido.';
@@ -82,6 +85,14 @@ export class LocalizarModalComponent implements OnInit {
 
   openDisponibilidadModal() { // Abre el modal de disponibilidad que está puesto en el localizar para agilizar la adquisición de una bóveda
     this.modalService.open(DisponibilidadModalComponent, { centered: true, size: 'lg' });
+  }
+
+  // Esta funcion formatea un número o string para que tenga al menos dos dígitos (añade un cero delante si es necesario)
+  // esto es importante para la correcta lectura de las imágenes de los bloques, ya que todas siguen un formato de dos dígitos
+  // lo que hace es verificar la longitud del valor convertido a string, y si es 1, añade un '0' delante, fin.
+  formatTwoDigits(value: number | string): string {
+    const strValue = value.toString();
+    return strValue.length === 1 ? `0${strValue}` : strValue;
   }
 
 
