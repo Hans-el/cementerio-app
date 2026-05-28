927d3d88dcb1a89660dc8b888b22239a5a074e6e
Hans-el <halcivar3030@utm.edu.ec>
2026-05-27 23:07:28 -0500

    Implementación de Analíticas en Vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
+
+inject();
+
 
 
 bootstrapApplication(AppComponent, appConfig)
