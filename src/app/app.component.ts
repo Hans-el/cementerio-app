4b4df95ff1f4a099c69fa1f1cbede2d8dac4a690
Hans-el <halcivar3030@utm.edu.ec>
2026-06-20 14:25:55 -0500

    Gif entre pantallas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CementerioService } from './services/cementerio.service';
-import { RouterOutlet } from '@angular/router';
+import {
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+  RouterOutlet
+} from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
-  template: `
-      <router-outlet></router-outlet>
-`,
+  imports: [RouterOutlet, LayoutComponent, CommonModule],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'cementerio-app';
   isSidebarCollapsed: boolean = false;
+  loading = false;
 
-  constructor(private cementerioService: CementerioService) { }
+  constructor(
+    private cementerioService: CementerioService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     // Restaurar el tema del cementerio al recargar la página
@@ -25,6 +35,22 @@ export class AppComponent implements OnInit {
     if (cementerio) {
       this.cementerioService.aplicarTema(cementerio);
     }
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      }
+
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        setTimeout(() => {
+          this.loading = false;
+        }, 700);
+      }
+    });
   }
   onToggleSidebar(collapsed: boolean): void {
     this.isSidebarCollapsed = collapsed;
