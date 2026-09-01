8cee85af2e18f921b4f81b755ee0a0482c973d32
Hans-el <halcivar3030@utm.edu.ec>
2026-08-31 20:39:38 -0500

    Nuevo logo & favicon.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://cementerios-portoviejo.duckdns.org/api', //prod => usando oracle (usando duckdns)
-  // apiUrl: 'http://localhost:3000/api', //para desarrollo
+  // apiUrl: 'https://cementerios-portoviejo.duckdns.org/api', //prod => usando oracle (usando duckdns)
+  apiUrl: 'http://localhost:3000/api', //para desarrollo
 };
